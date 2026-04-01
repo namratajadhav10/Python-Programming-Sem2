9dfc106810485cf0a42e94931ad7ca7a75340b89
namratajadhav10 <namratajadhav02007@gmail.com>
2026-04-01 16:38:44 +0530

    Add files via upload

---
+++ exp3 sample1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 11:07:06 2026
+
+@author: namrata jadhav
+"""
+
+n=int(input("Enter number of rows:"))
+
+for i in range (1,n + 1):
+    for j in range(1,i+ 1):
+        print(j,end="")
+        print()
